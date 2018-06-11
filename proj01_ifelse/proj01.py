a1efa811f174302e25f5d6da3aea22c3016ccfae
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2018-06-11 16:01:36 -0400

    Update proj01.py

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -10,9 +10,9 @@
 #Then, it prints out a sentence that says the number of years until they graduate.
 
 
-#name = input("What's your name?")
-#grade = int(input("What grade are you in?"))
-#print(name[0].upper() + name[1:].lower() + " will graduate in " + str(12 - grade) + " years.")
+name = input("What's your name?")
+grade = int(input("What grade are you in?"))
+print(name[0].upper() + name[1:].lower() + " will graduate in " + str(12 - grade) + " years.")
 
 
 # Part II:
@@ -28,21 +28,21 @@ name = input("What is your name?")
 birthday = int(input("When is your birthday?"))
 birth_month = int(input("When is your birth month?"))
 
-#Math for remaining months
+#Remaining months
 if birth_month >= current_month:
     months_left = 12 - (birth_month - current_month)
 elif birth_month < current_month:
     months_left = 12 - (current_month - birth_month)
 
-#Math for remaining days
+#Remaining days
 if birthday >= current_day:
     days_left = birthday - current_day
 elif birthday < current_day:
     days_left = 30 - (current_day - birthday)
     months_left = months_left - 1
 
-print(name + ", your next birthday is in " + str(days_left) + "days and " + str(months_left) + "months")
-
+#Final String
+print(name[0].upper() + name[1:].lower() + ", your next birthday is in " + str(days_left) + " days and " + str(months_left) + " months")
 
 
 # If you complete extensions, describe your extensions here!
