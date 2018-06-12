70908f5cbbd50e9ac42a504fa67b45eab007ae39
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2018-06-12 16:01:23 -0400

    Update proj02_02.py

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -4,10 +4,56 @@
 # proj02_02: Fibonaci Sequence
 
 """
-Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
-sequence is a sequence of numbers where the next number in the sequence is the sum of the 
-previous two numbers in the sequence. The sequence looks like this: 
+Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci
+sequence is a sequence of numbers where the next number in the sequence is the sum of the
+previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
+
+"""#Loop Variable
+user_num = int(input("How many numbers would you like to generate from the fibonacci sequence?"))
+current_num = 1
+past_num = 0
+n = 0
+
+
+
+for num1 in range(0,user_num):
+   print(str(current_num))
+   next_num = past_num + current_num
+   past_num = current_num
+   current_num = next_num
+
+user_num1 = int(input("How many powers of 2 do you want"))
+current_num = 2
+
+
+for num1 in range(0,user_num1):
+   print(str(current_num))
+   next_num = current_num*2
+   current_num = next_num
+
+while n < user_num:
+    print(str(current_num))
+    next_num = past_num + current_num
+    past_num = current_num
+    current_num = next_num
+    n = n + 1
+
+user_num2 = int(input("What number to you want to find divisors?"))
+
+
+for divisor in range(1,user_num2):
+   if user_num2%divisor == 0:
+       print(str(divisor))"""
+
+
+
+
+
+
+
+
+
 
 
